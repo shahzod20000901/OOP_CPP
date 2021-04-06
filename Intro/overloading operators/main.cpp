7f64acea11ad9aa26b07622c829d3d4be4f2080b
Shahzod <kudratovsahzod2@gmail.com>
2021-04-06 20:44:44 +0300

    operator + and - done

--- Intro/overloading operators/main.cpp
+++ Intro/overloading operators/main.cpp
@@ -8,10 +8,16 @@ using std::endl;
 class Fraction
 {
 	double x;
+	double y;
 public:
 	double get_x()const
 	{
 		return x;
+	}
+
+	double get_y()const
+	{
+		return y;
 	}
 
 	void set_x(double x)
@@ -19,15 +25,22 @@ public:
 		this->x = x;
 	}
 
-	Fraction(double x=0)
+	void set_y(double y)
+	{
+		this->y = y;
+	}
+
+	Fraction(double x=0, double y = 0)
 	{
 		this->x = x;
+		this->y = y;
 		cout << "Constructor: " << this << endl;
 	}
 
 	Fraction(const Fraction& other)
 	{
 		this->x = other.x;
+		this->y = other.y;
 		cout << "CopyConstructor: " << this << endl;
 	}
 
@@ -40,28 +53,67 @@ public:
 		return value;
 	}
 
-	Fraction reduce( Fraction& other)  //5/10=>1/2
+	Fraction operator+(const Fraction& other)
 	{
-		double value = this->x;
-		double result = other.x;
+		Fraction result;
+		if (this->y == other.y)
+		{
+			result.set_x(this->x + other.x);
+			result.set_y(this->y);
+		}
+		else if (this->y != other.y)
+		{
+			result.set_x(this->x * other.y + other.x * this->y);
+			result.set_y(this->y * other.y);
+		}
+		return result;
+	}
+
+	Fraction operator-(const Fraction& other)
+	{
+		Fraction result;
+		if (this->y == other.y)
+		{
+			result.set_x(this->x - other.x);
+			result.set_y(this->y);
+		}
+		else if (this->y != other.y)
+		{
+			result.set_x(this->x * other.y - other.x * this->y);
+			result.set_y(this->y * other.y);
+		}
+		return result;
+	}
+
+	Fraction& reduce(Fraction&)  //5/10=>1/2
+	{
+		int value = this->x;
+		int result =this->y;
 		if (value < result)
 		{
 			while (value % value != 0 || result % value != 0)
 			{
 				--value;
 			}
-			this->x /= value;
-			other.x /= value;
+			this->set_x(this->x /= value);
+			this->set_y(this->y /= value);
 		}
-		else
+		else if(result<value)
 		{
-			while (result % result != 0 && value % result != 0)
+			while (result % result != 0 || value % result != 0)
 			{
 				--result;
 			}
-			this->x /= result;
-			other.x /= result;
+			this->set_x(this->x /= result);
+			this->set_y(this->y /= result);
 		}
+		return *this;
+	}
+
+
+	void print() const
+	{
+		cout << x << "/" << y << endl;
 	}
 
 	~Fraction()
@@ -79,7 +131,7 @@ istream& operator>>(istream& is, Fraction& obj)
 
 ostream& operator<< (ostream& os, Fraction& obj)
 {
-	os << obj.get_x() << endl;
+	os << obj.get_x() <<"/"<<obj.get_y()<< endl;
 	return os;
 }
 
@@ -90,8 +142,22 @@ void main()
 	Fraction B;
 	A.set_x(19);
 	B.set_x(18);
+	A.print();
+	B.print();
 	Fraction C = A / B;
+	C.print();
 	cout << C << endl;
+
+	Fraction D;
+	D.set_x(16);
+	D.set_y(26);
+	D.print();
+	Fraction E;
+	E.set_x(4);
+	E.set_y(25);
+	E.print();
+	Fraction F = D + E;
+	cout <<F << endl;
 }
 
 //TODO:
